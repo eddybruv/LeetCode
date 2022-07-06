7ff6e731a9ed0305706a5aecb0bfa0d89fd16a53
Edwin Ajong <edwinajong@gmail.com>
2022-07-06 05:22:57 +0100

    Time: 306 ms (39.70%), Space: 51.8 MB (61.44%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.ts
+++ 9-palindrome-number/9-palindrome-number.ts
@@ -1,19 +1,11 @@
 function isPalindrome(x: number): boolean {
   let num: number = x;
   let revX: number = 0;
-  let ex = x;
-  let n = -1;
-  
-  while (ex >= 1) {
-    ex /= 10;
-    n++;
-  }
   
   while (num >= 1) {
     let rem:number = Math.floor(num % 10);
     num /= 10;
-    revX = (rem * Math.pow(10, n)) + revX;
-    n--;
+    revX = revX*10 + rem;
   }
   
   if(revX === x)
