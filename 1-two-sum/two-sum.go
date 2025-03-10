fcdc1b69cc76e54f7b51033c92e9b190ccb46891
Edwin Ajong <edwinajong@gmail.com>
2025-03-09 23:05:22 -0500

    Time: 1 ms (57.64%) | Memory: 6 MB (41.07%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -3,15 +3,11 @@ func twoSum(nums []int, target int) []int {
 
     for i, value:= range nums {
         find := target - value;
-
-        fmt.Println(indexMap[find])
-
-        _, ok := indexMap[find]
-        if ok {
+        if _, ok := indexMap[find]; ok {
             return []int{indexMap[find], i}
         }
 
         indexMap[value] = i
     }
-    return []int{0, 0}
+    return nil
 }
